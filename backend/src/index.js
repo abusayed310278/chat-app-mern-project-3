7d7449612310caf95677ea3c00bf9c7c666c72a5
Abu Sayed <83999377+abusayed310278@users.noreply.github.com>
2025-05-22 19:51:02 +0600

    theme setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,29 +1,30 @@
+import express from "express";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
-import express from'express'
-import authRoutes from './routes/auth.route.js'
-import messageRoutes from './routes/message.route.js'
-import dotenv from 'dotenv'
-import {connectDB} from './lib/db.js'
-import cookieParser from 'cookie-parser'
-import cors from 'cors'
+dotenv.config();
+const app = express();
 
-dotenv.config()
-const app=express();
-
-const PORT=process.env.PORT || 5001;
+const PORT = process.env.PORT || 5001;
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
-}))
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
-app.use("/api/auth",authRoutes);
-app.use("/api/message",messageRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`);
-    connectDB()
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  connectDB();
 });
